f1e94351d8b4545883bb50b4702c202b89d50c23
zahramkd95 <57577157+zahramkd95@users.noreply.github.com>
2025-03-23 23:23:39 -0400

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,9 +1,9 @@
 import logging 
 
-#Adding logger to post to console ouptut with clear timestamps, levels and messages. 
+##Adding logger to post to console ouptut with clear timestamps, filenames, line numbers, levels and messages. 
 
 logging.basicConfig(
     level=logging.INFO,  # Minimum level to log 
-    format='%(asctime)s - %(levelname)s - %(message)s',  # Log format
+    format='%(asctime)s - %(levelname)s - %(filename)s - %(lineno)d - %(message)s',  # Log format
     handlers=[logging.StreamHandler()]  # Stream handler to post in console
 )
